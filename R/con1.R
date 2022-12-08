951950fee204ca80b6bb710484902b8687bedea5
freesebecca <frees048@umn.edu>
2022-12-08 13:26:40 -0600

    Init bdacR

---
+++ R/con1.R
@@ -0,0 +1,32 @@
+#' One Continuous Variable
+#'
+#' This function allows you to summarize a continuous variable for tables
+#' @param y Variable to summarize
+#' @param l Label for summary column. Defaults to Overall
+#' @param prmr Include median and range? Defaults to FALSE
+#' @param nam Label of summary variable
+#' @keywords
+#' @export
+#' @examples
+#' con1()
+
+con1 <- function(y, l = "Overall", prmr = FALSE, nam = "") {
+  if (prmr == TRUE) {
+    matrix(
+      c("", sprintf("%1.1f (%1.2f) [%1.0f]", mean(y, na.rm = T), sd(y, na.rm = T), sum(is.na(y))),
+        sprintf("%1.1f [%1.1f, %1.1f]", median(y, na.rm = T), min(y, na.rm = T), max(y, na.rm = T))
+      ),
+      nrow = 3, byrow = T, dimnames = list(c(
+        nam,
+        "&nbsp;&nbsp;Mean (SD) [NA]",
+        "&nbsp;&nbsp;Median [Range]"
+      ), l))
+  } else {
+    matrix(
+      c("", sprintf("%1.2f (%1.2f) [%1.0f]", mean(y, na.rm = T), sd(y, na.rm = T), sum(is.na(y)))
+      ),
+      nrow = 2, byrow = T, dimnames = list(c(
+        nam, "&nbsp;&nbsp;Mean (SD) [NA]"
+      ), l))
+  }
+}
---
+++ R/con2.R
@@ -0,0 +1,50 @@
+#' Grouped Continuous Variable
+#'
+#' This function allows you to summarize a continuous variable by group for tables
+#' @param x Grouping variable
+#' @param y Variablet o summarize per group
+#' @param d Digits to print. Defaults to 1
+#' @param nam Lable of characteristic column. Defaults to ""
+#' @param prmr Include median and range? Defaults to FALSE.
+#' @keywords
+#' @export
+#' @examples
+#' con2()
+
+con2 <- function(x, y, d = 1, nam = "", prmr = TRUE) {
+  if (prmr == TRUE) {
+    rbind(
+      matrix("", ncol = length(levels(as.factor(x))), dimnames = list(nam)),
+      matrix(
+        c(by(y, x, function(j) {
+          sprintf(paste0("%1.", d, "f (%1.", d, "f) [%1.0f]"),
+                  mean(j, na.rm = TRUE),
+                  sd(j, na.rm = TRUE),
+                  sum(is.na(j)))
+        }),
+        by(y, x, function (k) {
+          sprintf(paste0("%1.", d, "f [%1.", d, "f, %1.", d, "f]"),
+                  median(k, na.rm = T),
+                  min(k, na.rm = T),
+                  max(k, na.rm = T))
+        })),
+        nrow = 2, byrow = T, dimnames = list(c(
+          "&nbsp;&nbsp;Mean (SD) [NA]",
+          "&nbsp;&nbsp;Median [Range]"
+        ), levels(as.factor(x)))))
+  } else {
+    rbind(
+      matrix("", ncol = length(levels(as.factor(x))), dimnames = list(nam)),
+      matrix(
+        c(by(y, x, function(j) {
+          sprintf(paste0("%1.", d, "f (%1.", d, "f) [%1.0f]"),
+                  mean(j, na.rm = T),
+                  sd(j, na.rm = T),
+                  sum(is.na(j)))
+        })),
+        nrow = 1, byrow = T, dimnames = list(c(
+          "&nbsp;&nbsp;Mean (SD) [NA]"
+        ), levels(as.factor(x)))))
+  }
+}
+

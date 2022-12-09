f0ed9e4016fee09fb74129db5af417e6005eade6
freesebecca <frees048@umn.edu>
2022-12-09 13:27:41 -0600

    Add dataset with color lookup for UMN branding

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' UMN Brand standard color look up
+#'
+#' A subset of data from the World Health Organization Global Tuberculosis
+#' Report ...
+#'
+#' @format ## `UMN_colors`
+#' A data frame with 12 rows and 3 columns:
+#' \describe{
+#'   \item{Name}{Color name}
+#'   \item{Type}{Primary or Secondary color}
+#'   \item{HEX}{HEX color code}
+#'   ...
+#' }
+#' @source <https://university-relations.umn.edu/resources/colors-and-type>
+"UMN_colors"
